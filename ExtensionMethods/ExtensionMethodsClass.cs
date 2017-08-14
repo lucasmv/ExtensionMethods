334a8670f51d5744a1eaa2e93e2b85c0b73662c3
Casa <Casa@DESKTOP-9TDPC3M>
2017-08-14 20:06:27 -0300

    Add project files.

---
+++ ExtensionMethods/ExtensionMethodsClass.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ExtensionMethods
+{
+    public static class ExtensionMethodsClass
+    {
+        public static string ToMD5(this string value)
+        {
+            System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+
+            byte[] inputBytes = Encoding.ASCII.GetBytes(value);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+                sb.Append(hash[i].ToString("X2"));
+
+            return sb.ToString();
+        }
+
+        public static decimal CalcularIPI(this decimal value, decimal percentual)
+        {
+            decimal Total = decimal.Divide(decimal.Multiply(value, percentual), 100);
+            return Total;
+        }
+
+        public static string Abreviacao(this string value)
+        {
+            var  pattern = "(de\\.? |do\\.? |dos |da\\.? |das\\.? )";
+
+            var nome = Regex.Replace(value, pattern, string.Empty);
+            var nomeAbreviado = string.Empty;
+            var nomeQuebrado = nome.Split(' ');
+
+            foreach (var item in nomeQuebrado)
+                nomeAbreviado += item[0] + " ";
+
+            return nomeAbreviado;
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region MD5
+
+            string Nome = "C# Brasil";
+            string MD5 = Nome.ToMD5();
+
+            Console.WriteLine(MD5);
+
+            #endregion
+
+            #region Calcula IPI
+
+            decimal Valor = 580.78M;
+            decimal Perc = 2;
+
+            decimal IPI = Valor.CalcularIPI(Perc);
+
+            Console.WriteLine(IPI.ToString());
+
+            #endregion
+
+            #region Abreviar Nome
+
+            var nome1 = "Lucas Magno Veiga Silva";
+            var nome2 = "Jose Antonio da Silva";
+
+            Console.WriteLine(nome1.Abreviacao());
+            Console.WriteLine(nome2.Abreviacao());
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
